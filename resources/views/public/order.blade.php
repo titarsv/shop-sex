5a80578e4cf0dffff78e2dceb72154b20ed97ff9
Tit@r <titarsv@yandex.ru>
2023-07-23 19:48:41 +0300

    checkout

--- resources/views/public/order.blade.php
+++ resources/views/public/order.blade.php
@@ -25,7 +25,7 @@
                             <div class="cart__item">
                                 <div class="cart__item-delete" data-prod-id="{{ $code }}"><div></div></div>
                                 <div class="cart__item-img">
-                                    <img src="{{ is_null($product['product']->image) ? '/uploads/no_image.jpg' : $product['product']->image->url('cart') }}" alt="{{ $product['product']->name }}" alt="">
+                                    <img src="{{ is_null($product['product']->image) ? '/uploads/no_image.jpg' : $product['product']->image->url('cart') }}" alt="{{ $product['product']->name }}">
                                 </div>
                                 <div class="cart__item-name">
                                     <p>
@@ -54,8 +54,8 @@
                 <div class="col-sm-4 col-xs-12" id="liqpay_checkout">
                     <p class="cart-form__text">{{ trans('app.checkout') }}</p>
                     <form action="{{env('APP_URL')}}/order/create" method="post" class="cart-form" id="order-checkout">
-                        <p class="cart-form__text">*{{ trans('app.your_contact_phone_number') }}</p>
-                        <input class="cart-form__input clear-styles" type="tel" name="phone" data-title="{{ trans('app.phone') }}" data-validate-required="{{ trans('app.obligatory_field') }}" data-validate-uaphone="Неправильный номер" placeholder="+380(___)-__-__-__" required>
+                        <p class="cart-form__text">{{ trans('app.your_contact_phone_number') }}<br><small style="font-size: 12px;">* {{ trans('app.obligatory_field') }}</small></p>
+                        <input class="cart-form__input clear-styles" type="tel" name="phone" data-title="{{ trans('app.phone') }}" data-validate-required="{{ trans('app.obligatory_field') }}" data-validate-uaphone="Неправильный номер" placeholder="+380(___)-__-__-__">
                         <div class="cart-block_checkout">
                             <div class="error-message__text" style="font-size: 12px"></div>
                         </div>
